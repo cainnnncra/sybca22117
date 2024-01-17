6e242770e5545fba58c8bb8f124245830b6270f5
Cain Crasto <caincrasto123@gmail.com>
2024-01-17 09:55:54 +0530

    addition

---
+++ what/src/what/num.java
@@ -0,0 +1,15 @@
+package what;
+
+public class num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int num1= 5;
+		 int num2= 10;
+		 int sum=num1+num2;
+		 System.out.println("The sum is"+ sum);
+
+	}
+
+}
